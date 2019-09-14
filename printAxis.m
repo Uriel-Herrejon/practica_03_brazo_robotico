a516c8f769480a9eaeaa3da27ed735bf086a8509
Uriel Herrejón <54286298+Uriel-Herrejon@users.noreply.github.com>
2019-09-13 19:54:17 -0500

    Add files via upload

---
+++ printAxis.m
@@ -0,0 +1,4 @@
+function printAxis()
+line([-10 10],[0 0],[0 0],'color',[1 0 0],'linewidth',2.5); %Establece el eje en x rojo
+line([0 0],[-10 10],[0 0],'color',[0 1 0],'linewidth',2.5); %Establece el eje en y verde
+end
